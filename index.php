f4462283d4306971ade90dd8f451f7c774823b0c
Jayesh9819 <131602888+Jayesh9819@users.noreply.github.com>
2024-01-16 08:17:04 +0000

    chsnges

--- index.php
+++ index.php
@@ -76,7 +76,7 @@
                             
         </form>
         </div>
-        <a href="Login.php">Admin Login</a>
+        <a href="admin.php">Admin Login</a>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </body>
--- vender.php
+++ vender.php
@@ -1,12 +1,15 @@
 <?php
+include "config.php";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Get the QR code data from the AJAX request
     $qrCodeData = $_POST['qrCode'];
+    $sql="INSERT INTO QR";
+
+    echo $qrCodeData;
 
     echo "Received QR Code Data: $qrCodeData";
 } else {
-    // Handle invalid requests
     http_response_code(400);
     echo "Invalid request method";
 }
