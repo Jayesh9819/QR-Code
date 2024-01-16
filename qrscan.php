4c2ebf7409da65a9cc2d2627bcd956487a15ed52
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-01-16 21:11:26 +0530

    Update qrscan.php

--- qrscan.php
+++ qrscan.php
@@ -3,7 +3,7 @@ session_start();
 if (isset($_GET['qrCode'])) {
     // Get the QR code data from the AJAX request
     $qrCodeData = $_GET['qrCode'];
-    $venderid=$_SESSION['id'];
+    $venderid=$_SESSION['User_id'];
     $sql="INSERT into qr_scanned VALUES ('',$venderid,$qrCodeData,'')";
     if ($mysqli->query($sql) === true) {
         // Qrcode::png($pname, $file, $ecc, $pixel_Size, $frame_Size);
